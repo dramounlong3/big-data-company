49a6141ad97e54bbc9ec7ed54e226f6aa2d8ba6c
Kyle Guo <dramounlong3@gmail.com>
2022-02-15 13:10:43 +0800

    20220215 heroku frontend government data api v1

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static("client/css"));
+app.use(cors());
+
+//port 號會由 Heroku 給予，因此不再自行指定
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/index.html"));
+});
+
+app.get("/getData", (req, res) => {
+  //台北市各地區共同生活戶資料
+  async function getData() {
+    // 取資料
+    console.log("A request is comming.");
+    fetch(
+      "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000082-045"
+    ).then((response) => {
+      response
+        .text()
+        .then((population) => {
+          console.log("Finsih.");
+          res.send(population);
+        })
+        .catch((e) => {
+          console.log(e.response);
+        });
+    });
+  }
+  getData();
+});
+
+app.get("*", (req, res) => {
+  res.send("The URL is not exist.");
+});
+
+app.listen(port, () => {
+  console.log("the server is running on port 8000.");
+});
