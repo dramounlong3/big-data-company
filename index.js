3b96dd587376e8cb194b2fa8c3b67b82f8864a69
Kyle Guo <dramounlong3@gmail.com>
2022-02-15 13:50:48 +0800

    20220215 heroku frontend government data api v4

--- index.js
+++ index.js
@@ -12,49 +12,31 @@ app.use(cors());
 //port 號會由 Heroku 給予，因此不再自行指定
 const port = process.env.PORT || 8000;
 
-async function getData() {
-  // 取資料
-  console.log("A request is comming.");
-  fetch(
-    "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000082-045"
-  ).then((response) => {
-    response
-      .text()
-      .then((population) => {
-        console.log("Finsih.");
-        console.log(population);
-      })
-      .catch((e) => {
-        console.log(e.response);
-      });
-  });
-}
-getData();
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/index.html"));
 });
 
 app.get("/getData", (req, res) => {
   //台北市各地區共同生活戶資料
-  // async function getData() {
-  //   // 取資料
-  //   console.log("A request is comming.");
-  //   fetch(
-  //     "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000082-045"
-  //   ).then((response) => {
-  //     response
-  //       .text()
-  //       .then((population) => {
-  //         console.log("Finsih.");
-  //         res.send(population);
-  //       })
-  //       .catch((e) => {
-  //         console.log(e.response);
-  //       });
-  //   });
-  // }
-  // getData();
+  async function getData() {
+    // 取資料
+    console.log("A request is comming.");
+    fetch(
+      //"https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000082-045"
+      "https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=99D563D2-568C-4FA2-B4B0-1783453B1136"
+    ).then((response) => {
+      response
+        .text()
+        .then((population) => {
+          console.log(population);
+          res.send(population);
+        })
+        .catch((e) => {
+          console.log(e.response);
+        });
+    });
+  }
+  getData();
 });
 
 app.get("*", (req, res) => {
@@ -62,5 +44,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("the server is running on port 8000.");
+  console.log(`the server is running on port ${port}.`);
 });
